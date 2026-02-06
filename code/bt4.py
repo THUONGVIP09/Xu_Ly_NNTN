9bc7085c0cfc09152a23116ac7f7d4d44ee4c4b1
THUONGVIP09 <email@gmail.com>
2026-02-06 13:46:57 +0700

    init4

--- code/bt4.py
+++ code/bt4.py
@@ -1,45 +1,52 @@
-# documents = ["Dog bites man.", "Man bites dog.", "Dog eats meat.", "Man eats food."]
-# processed_docs = [doc.lower().replace(".","") for doc in documents]
-# print(processed_docs)
+documents = [
+    "Student studies math.",
+    "Teacher explains lesson.",
+    "Computer runs program.",
+    "Model predicts result."
+]
+
+processed_docs = [doc.lower().replace(".","") for doc in documents]
+print(processed_docs)
 
 
-# vocab = {}
-# count = 0
-# for doc in processed_docs:
-#     for word in doc.split():
-#         if word not in vocab:
-#             count = count +1
-#             vocab[word] = count
-# print(vocab)
+vocab = {}
+count = 0
+for doc in processed_docs:
+    for word in doc.split():
+        if word not in vocab:
+            count = count +1
+            vocab[word] = count
+print(vocab)
 
 
-# def get_onehot_vector(somestring):
-#     onehot_encoded = []
-#     for word in somestring.split():
-#         temp = [0]*len(vocab)
-#         if word in vocab:
-#             temp[vocab[word]-1] = 1 # -1 is to take care of the fact indexing in array starts from 0 and not 1
-#         onehot_encoded.append(temp)
-#     return onehot_encoded
-# print(processed_docs[1])
-# get_onehot_vector(processed_docs[1])
+def get_onehot_vector(somestring):
+    onehot_encoded = []
+    for word in somestring.split():
+        temp = [0]*len(vocab)
+        if word in vocab:
+            temp[vocab[word]-1] = 1 # -1 is to take care of the fact indexing in array starts from 0 and not 1
+        onehot_encoded.append(temp)
+    return onehot_encoded
+print(processed_docs[1])
+get_onehot_vector(processed_docs[1])
 
-# print(get_onehot_vector(processed_docs[1]))
+print(get_onehot_vector(processed_docs[1]))
 
-# get_onehot_vector("man and dog are good") 
-# print(get_onehot_vector("man and dog are good"))
-
-
-
-# get_onehot_vector("man and man are good") 
-# print(get_onehot_vector("man and man are good"))
+get_onehot_vector("man and dog are good") 
+print(get_onehot_vector("man and dog are good"))
 
 
 
-S1 = 'dog bites man'
-S2 = 'man bites dog'
-S3 = 'dog eats meat'
-S4 = 'man eats food'
+get_onehot_vector("man and man are good") 
+print(get_onehot_vector("man and man are good"))
+
+
+
+S1 = 'student studies math'
+S2 = 'teacher explains lesson'
+S3 = 'computer runs program'
+S4 = 'model predicts result'
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 data = [S1.split(), S2.split(), S3.split(), S4.split()]
